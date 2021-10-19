1b06e94f2443a018cef18a04ef7a1d5acb9d2213
Belom7 <Still07pkm@gmail.com>
2021-10-19 23:57:21 +0300

    completed the task 81

---
+++ lesson_81.js
@@ -0,0 +1,9 @@
+var myDog = {
+    // Only change code below this line
+    'name': 'Maks',
+    'legs': 4,
+    'tails': 55,
+    'friends': ['Ivan', 'Andrew']
+
+    // Only change code above this line
+};
